0939d45df5940c3006a793de1ddc91bd50eadf2b
mikeeg555 <mikeeg555@gmail.com>
2022-06-01 15:18:20 -0700

    Add files via upload
    fixed issue with cumulative extrusion resets

--- layerRepeater.py
+++ layerRepeater.py
@@ -62,7 +62,7 @@ def main(filename, layerToRepeat, numReps):
     zHeightToRepeat = round(LAYER_HEIGHT * layerToRepeat, 1)
     deltaZmm = round(LAYER_HEIGHT * numReps, 1)
 
-    print(f"Layer {layerToRepeat} to be repeated {numReps} times. adding total {deltaZmm} mm to model.")
+    print(f"Layer {layerToRepeat} to be repeated {numReps} times. adding total {deltaZmm} mm to model height.")
     lineCount=0
     layerCount=0
 
@@ -71,31 +71,43 @@ def main(filename, layerToRepeat, numReps):
 
     # first go through file and convert all E values to relative instead of cummulative/absolute
     #  find the last extruder reset
-    lineNum = 0
-    firstExtLine = 0
-    lastExtruderResetLine = 0
-    for line in fileLines:
-        finalExtruderReset = re.findall('G92', line)
-        if len(finalExtruderReset) > 0:
-            # found an extruder reset
-            lastExtruderResetLine = lineNum
-        lineNum += 1
+    # lineNum = 0
+    # firstExtLine = 0
+    # lastExtruderResetLine = 0
+    # for line in fileLines:
+    #     finalExtruderReset = re.findall('G92', line)
+    #     if len(finalExtruderReset) > 0:
+    #         # found an extruder reset
+    #         lastExtruderResetLine = lineNum
+    #         print(f"found extruder reset at line {lineNum+1}")
+    #     lineNum += 1
     # print(f"Last extruder reset line:{lastExtruderResetLine}")
 
     # now start after last reset and convert all extrusion amounts to relative
     absExtAmount = 0.0
-    lineNum = lastExtruderResetLine + 1
+    numRelChanges = 0
+    lineNum = 0
+    totalExtrusionBefore = 0
     for line in fileLines[lineNum:]:
-        extrusionAmtFound = re.findall('E[0-9]{1,6}\.[0-9]{0,6}', line)
-        if len(extrusionAmtFound) > 0:
-            # found a line with extrusion command
-            extAmtFloat = float(extrusionAmtFound[0][1:])
-            relExtAmtFloat = round(extAmtFloat - absExtAmount, 5)
-            newLineString = swapAbsExtForRel(line, extrusionAmtFound[0], relExtAmtFloat)
-            fileLines[lineNum] = newLineString
-            # set the new abs amount for next iteration
-            absExtAmount = extAmtFloat
+        extruderReset = re.findall('G92', line)
+        if len(extruderReset) > 0:
+            absExtAmount = 0.0 # the extruder has been reset, we must also reset our absolute amount
+            # print(f"reset extruder amt at line {lineNum+1}")
+        else:
+            extrusionAmtFound = re.findall('E[0-9]{1,6}\.[0-9]{0,6}', line)
+            if len(extrusionAmtFound) > 0:
+                # found a line with extrusion command
+                extAmtFloat = float(extrusionAmtFound[0][1:])
+                relExtAmtFloat = round(extAmtFloat - absExtAmount, 5)
+                totalExtrusionBefore += relExtAmtFloat # keep track of total file extrusion for before/after comparison
+                newLineString = swapAbsExtForRel(line, extrusionAmtFound[0], relExtAmtFloat)
+                fileLines[lineNum] = newLineString
+                # set the new abs amount for next iteration
+                absExtAmount = extAmtFloat
+                numRelChanges += 1
         lineNum += 1
+
+    print(f"Changed to relative: {numRelChanges} lines")
 
     # find all Z height changes and shift later layers by deltaZmm
     lineNum = 0
@@ -142,28 +154,38 @@ def main(filename, layerToRepeat, numReps):
         outputFileLines.append(repeatedLayerLines[0]) # add comment line
         outputFileLines.append(newFirstLine) # add modded command line
         outputFileLines += repeatedLayerLines[2:] # add rest of layer chunk
-        # print(f"copied layer {layerToRepeat} code up to line {len(outputFileLines)}")
+        print(f"copied layer {layerToRepeat} code up to line {len(outputFileLines)}")
 
     # now add the rest of the layers, each already offset by deltaZmm
     outputFileLines += fileLines[nextLayerLineNum:]
-    # print(f"added final {len(fileLines[nextLayerLineNum:])} lines")
+    print(f"added final {len(fileLines[nextLayerLineNum:])} lines")
 
     # now convert extrusion amounts back to absolute
     # Note: extrusion command 'E' has been replaced with "Erel"
     # so it is easy to find all the ones we touched before
-    lineNum = lastExtruderResetLine + 1
+    lineNum = 0
     cummulativeExt = 0
+    totalExtrusionAfter = 0
     for line in outputFileLines[lineNum:]:
+        extruderReset = re.findall('G92', line)
+        if len(extruderReset) > 0:
+            cummulativeExt = 0.0 # the extruder has been reset, we must also reset our cummulative amount
+            # print(f"reset cummulitive extruder amt at line {lineNum+1}")
         # find the Erel commands we made earlier. Need to include optional '-' for negative (retractions)
-        extrusionAmtFound = re.findall('Erel-?[0-9]{1,3}\.[0-9]{0,6}', line)
-        if len(extrusionAmtFound) > 0:
-            relAmtFloat = float(extrusionAmtFound[0][4:])
-            cummulativeExt += relAmtFloat
-            newLineString = swapRelExtForAbs(line, extrusionAmtFound[0], cummulativeExt)
-            outputFileLines[lineNum] = newLineString
+        else:
+            extrusionAmtFound = re.findall('Erel-?[0-9]{1,3}\.[0-9]{0,6}', line)
+            if len(extrusionAmtFound) > 0:
+                relAmtFloat = float(extrusionAmtFound[0][4:])
+                totalExtrusionAfter += relAmtFloat # keep track of total file extrusion for before/after comparison
+                cummulativeExt += relAmtFloat
+                newLineString = swapRelExtForAbs(line, extrusionAmtFound[0], cummulativeExt)
+                outputFileLines[lineNum] = newLineString
         lineNum += 1
 
-    print(f"Total lines modified: {linesChanged}")
+    print(f"\nTotal lines modified: {linesChanged}")
+    print(f"Total filament before: {round(totalExtrusionBefore, 1)} mm")
+    print(f"Total filament after : {round(totalExtrusionAfter, 1)} mm")
+    print(f"Total added filament : {round(totalExtrusionAfter-totalExtrusionBefore, 1)} mm")
 
     # write to file output
     outFilename = "layerRepeaterOutput.gcode"
